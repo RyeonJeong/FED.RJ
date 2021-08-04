9ab1463acea64c12d02b2e8803ae4927a33968ba
ryeonjeong <fpalsl@naver.com>
2021-08-04 16:35:04 +0900

    ss

--- 006.파일럿PJ/002.구현소스/js/men.js
+++ 006.파일럿PJ/002.구현소스/js/men.js
@@ -1,6 +1,33 @@
 // 남자패션페이지 JS - men.js
 
-$(function(){ // jQB //
-  
+$(function () { /// jQB ////////////////////////////
 
-}); /////// jQB //
+  /// 부드러운 스크롤 호출!(제이쿼리 아님!)
+  startSS();
+
+  /////////////////////////////////////
+  // 배너에 스와이퍼 플러그인 적용하기 /// 
+  /////////////////////////////////////
+  
+  var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1,
+      spaceBetween: 0,
+      loop: true,
+      speed: 500,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+        // disableOnInteraction 
+        // - 건드린 후 멈춤 true / 다시 작동은 false
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    }); //////// swiper ///////////////
+
+}); ///////////// jQB ////////////////////////////
