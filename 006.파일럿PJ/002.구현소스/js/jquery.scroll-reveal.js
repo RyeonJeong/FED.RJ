9d815f61ed54a197cdc95a507c0258c825907255
ryeonjeong <fpalsl@naver.com>
2021-08-09 10:45:59 +0900

    ss

--- 006.파일럿PJ/002.구현소스/js/jquery.scroll-reveal.js
+++ 006.파일럿PJ/002.구현소스/js/jquery.scroll-reveal.js
@@ -1,33 +1,34 @@
 (function( $ ) {
 
-  $.fn.scrollReveal = function() {
+    $.fn.scrollReveal = function() {
 
-      //get viewport size
-      var windowHeight = $(window).height(),
-          windowWidth = $(window).width(),
-          initialScroll = $(window).scrollTop(),
-          items = $('.js-reveal'),
-          scroll;
+        //get viewport size
+        var windowHeight = $(window).height(),
+            windowWidth = $(window).width(),
+            initialScroll = $(window).scrollTop(),
+            items = $('.js-reveal'),
+            scroll;
 
-      //hide anything not in the viewport
-      items.each(function(){
-          if($(this).offset().top >= windowHeight){
-              $(this).addClass('js-reveal--hidden');
-          }
-      });
+        //hide anything not in the viewport
+        items.each(function(){
+            if($(this).offset().top >= windowHeight){
+                $(this).addClass('js-reveal--hidden');
+            }
+        });
 
-      //on scroll
-      $(window).scroll(function (event) {
-          //get the current scroll position
-          scroll = $(window).scrollTop();
-          //show anything that is now in view (scroll + windowHeight)
-          items.each( function(){
-              if($(this).offset().top <= (scroll + windowHeight)){
-                  $(this).removeClass('js-reveal--hidden');
-              }
-          });
-      });
+        //on scroll
+        $(window).scroll(function (event) {
+            //get the current scroll position
+            scroll = $(window).scrollTop();
+            //show anything that is now in view (scroll + windowHeight)
+            items.each( function(){
+                if($(this).offset().top <= (scroll + windowHeight)){
+                    $(this).removeClass('js-reveal--hidden');
+                }
+            });
+        });
 
-      return this;
-  };
+        return this;
+    };
 }( jQuery ));
+
