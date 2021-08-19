8542889c035910cbe7a0ee819492a65f162bd781
RyeonJeong <87288318+RyeonJeong@users.noreply.github.com>
2021-08-20 01:46:42 +0900

    ss

--- 2021.나의인트로사이트/최련정인트로/999.최련정인트로/js/autoScroll_pageAction.js
+++ 2021.나의인트로사이트/최련정인트로/999.최련정인트로/js/autoScroll_pageAction.js
@@ -161,97 +161,123 @@ $(function () { /// jQB ////////////////////////
 
 
   // 제이쿼리 코드블록 ////////////////////////
-  $(function () {
+$(function () {
 
-
-    // 팝업창내용 셋팅
-    let poptxt = [
-      [
-        "이름: 서울랜드_PJ",
-        "구분: PC, 반응형",
-        "페이지: 메인/ sns페이지, footer링크 연결작업",
-        "설명: 기존 서울랜드 페이지를 그리드작업하여 최대한 동일한 느낌으로 제작하였습니다."
-      ],
-      [
-        "이름: 보그_PJ",
-        "구분: PC, 반응형",
-        "페이지: 메인/sub메뉴 페이지",
-        "설명: 기존 보그 페이지를 참고하여 최대한 동일한 느낌으로 제작하였습니다."
-      ],
-      [
-        "이름: 디스커버리_PJ",
-        "구분: PC, 반응형",
-        "페이지: 메인/ 메뉴의남성페이지",
-        "설명: 디스커버리 페이지를 참고하여 제작하였고 메뉴의 남성페이지 부분의 이미지 슬라이드 부분만 따로 디자인하여 제작하였습니다"
-      ],
-      [
-        "이름: MY Item_PJ",
-        "구분: PC",
-        "페이지: 메인/서브메뉴/상단오른쪽 input 메뉴들",
-        "설명: 학원에서 배운 input을 활용해보기 위해 만든 페이지로 회원가입, 로그인페이지 등 디자인보다는 기능에 비중을 더 두어 제작하였습니다."
-      ],
-    ];
-
-    let popurl = [
-      "http://www.naver.com",
-      "http://www.google.com",
-      "http://www.nate.com",
-      "http://www.daum.net"
-    ];
-
-    ///// 팝업창 띄우기 /////////
-    // 대상: .gong li:first-child
-    $(".mpbx")
-      .click(function () {
-
-        //순번
-        let idx = $(this).index();
+  // 팝업창내용 셋팅
+  let poptxt = {
+    "myintro2":[
+      "이름: 서울랜드_PJ",
+      "구분: PC, 반응형",
+      "페이지: 메인/ sns페이지, footer링크 연결작업",
+      "설명: 기존 서울랜드 페이지를 그리드작업하여 최대한 동일한 느낌으로 제작하였습니다."
+    ],
+    "SeoulLand_main":[
+      "이름: 보그_PJ",
+      "구분: PC, 반응형",
+      "페이지: 메인/sub메뉴 페이지",
+      "설명: 기존 보그 페이지를 참고하여 최대한 동일한 느낌으로 제작하였습니다."
+    ],
+    "discoverymain2":[
+      "이름: 디스커버리_PJ",
+      "구분: PC, 반응형",
+      "페이지: 메인/ 메뉴의남성페이지",
+      "설명: 디스커버리 페이지를 참고하여 제작하였고 메뉴의 남성페이지 부분의 이미지 슬라이드 부분만 따로 디자인하여 제작하였습니다"
+    ],
+    "myitemmain":[
+      "이름: MY Item_PJ",
+      "구분: PC",
+      "페이지: 메인/서브메뉴/상단오른쪽 input 메뉴들",
+      "설명: 학원에서 배운 input을 활용해보기 위해 만든 페이지로 회원가입, 로그인페이지 등 디자인보다는 기능에 비중을 더 두어 제작하였습니다."
+    ],
+    "myitemmain":[
+      "이름: MY Item_PJ",
+      "구분: PC",
+      "페이지: 메인/서브메뉴/상단오른쪽 input 메뉴들",
+      "설명: 학원에서 배운 input을 활용해보기 위해 만든 페이지로 회원가입, 로그인페이지 등 디자인보다는 기능에 비중을 더 두어 제작하였습니다."
+    ],
+    "myitemmain":[
+      "이름: MY Item_PJ",
+      "구분: PC",
+      "페이지: 메인/서브메뉴/상단오른쪽 input 메뉴들",
+      "설명: 학원에서 배운 input을 활용해보기 위해 만든 페이지로 회원가입, 로그인페이지 등 디자인보다는 기능에 비중을 더 두어 제작하였습니다."
+    ],
+    "myitemmain":[
+      "이름: MY Item_PJ",
+      "구분: PC",
+      "페이지: 메인/서브메뉴/상단오른쪽 input 메뉴들",
+      "설명: 학원에서 배운 input을 활용해보기 위해 만든 페이지로 회원가입, 로그인페이지 등 디자인보다는 기능에 비중을 더 두어 제작하였습니다."
+    ],
+    "myitemmain":[
+      "이름: MY Item_PJ",
+      "구분: PC",
+      "페이지: 메인/서브메뉴/상단오른쪽 input 메뉴들",
+      "설명: 학원에서 배운 input을 활용해보기 위해 만든 페이지로 회원가입, 로그인페이지 등 디자인보다는 기능에 비중을 더 두어 제작하였습니다."
+    ]
+  };
+  
+      let popurl = {
+        "CGV":"../001.CGV_PJ/003.site/index.html",
+        "MYITEM":"../005.MyItem_PJ/02.Asset/index.html",
+        "DISCOVERY":"../004.DiscoveryPJ/002.구현소스/index.html",
+        "VOGUE":"../002.VOGUE_PJ/002.site/index.html",
+        "SEOULLEND":"../003.SeoulLand_PJ/asset/index.html",
+        "SEOULLEND":"../003.SeoulLand_PJ/asset/index.html",
+        "SEOULLEND":"../003.SeoulLand_PJ/asset/index.html"
+      };
+  
+      ///// 팝업창 띄우기 /////////
+      // 대상: .gong li:first-child
+      $(".swiper-slide img")
+        .click(function () {
+  
+          //순번
+          let idx = $(this).index();
+          // 호출확인
+          console.log("팝업띄워!" + idx);
+  
+          // 팝업 띄우기
+          // 대상: .pop, .popbg
+          $(".pop,.popbg").show();
+          // show()는 display를 보이게하는 메서드
+  
+          let isrc = $(this).attr("src");
+          console.log(isrc);
+  
+          $(".pop img").attr("src", isrc);
+  
+          // 클릭된 이미지의 alt
+          let ialt = $(this).attr("alt");
+  
+          // 열린팝업창의 텍스트설명
+          let nowpop = poptxt[ialt];
+          console.log(nowpop);
+  
+          $(".pop ul li").each(function(idx,ele){
+            // console.log(idx);
+            $(ele).text(nowpop[idx]);
+          });////////// each //////////////////
+  
+          $(".baro").html('<a href="'+popurl[ialt]+'" target="_blank">웹사이트 바로가기</a>');
+  
+        }); //////// click ///////////////
+  
+      /// 팝업 닫기버튼 클릭시 /////
+      // 대상: .bbx button
+      $(".bbx button").click(function () {
         // 호출확인
-        console.log("팝업띄워!" + idx);
-
-        // 팝업 띄우기
-        // 대상: .pop, .popbg
-        $(".pop,.popbg").show();
-        // show()는 display를 보이게하는 메서드
-
-        let isrc = $(".btns", this).css("background-image");
-        isrc = isrc.split("/"); //배열변수에 담긴다
-        isrc = isrc[isrc.length - 1]; //맨 마지막요소
-        isrc = isrc.replace("\")", "");
-        console.log(isrc);
-
-        $(".pop img").attr("src", "images/" + isrc);
-
-        // 열린팝업창의 텍스트설명
-        let nowpop = poptxt[idx];
-        console.log(nowpop);
-
-        $(".pop ul li").each(function (idx, ele) {
-          //console.log(idx);
-          $(ele).text(nowpop[idx]);
-        }); ////////// each //////////////////
-
-        $(".baro").html('<a href="' + popurl[idx] + '" target="_blank">웹사이트 바로가기</a>');
-
-      }); //////// click ///////////////
-
-    /// 팝업 닫기버튼 클릭시 /////
-    // 대상: .bbx button
-    $(".bbx button").click(function () {
-      // 호출확인
-      console.log("팝업닫어!");
-
-      // 팝업 닫기
-      // 대상: .pop
-      $(".pop,.popbg").hide();
-      // hide()는 display:none으로 만들어주는 메서드
-
-    }); //////// click //////////////////
-
-
-
-  }); /////// jQB ///////////////////////////
-  ///////////////////////////////////////////
+        console.log("팝업닫어!");
+  
+        // 팝업 닫기
+        // 대상: .pop
+        $(".pop,.popbg").hide();
+        // hide()는 display:none으로 만들어주는 메서드
+  
+      }); //////// click //////////////////
+  
+  
+  
+    }); /////// jQB ///////////////////////////
+    ///////////////////////////////////////////
 
   // pageAction함수 최초호출!
   pageAction();
